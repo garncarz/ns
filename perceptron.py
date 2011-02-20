b42ed163e5392ae89511943628ccfd41ea16ff30
Ondřej Garncarz <ondrej@garncarz.cz>
2011-02-20 18:14:24 +0000

    Pokračování práce na perceptronu.

--- perceptron.py
+++ perceptron.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python2
 # coding=utf-8
 
-import pygtk
 import gtk, cairo
 import sys
 
-from math import ceil
+from math import ceil, pi
 
 class Net:
 
@@ -51,47 +50,130 @@ class Net:
 
 class Graph(gtk.DrawingArea):
 	
-	__gsignals__ = {"expose-event": "override"}
-
-	def do_expose_event(self, event):
-		self.draw(*self.window.get_size())
-	
-	def draw(self, width, height):
-		content = self.window.cairo_create()
-	
-		content.set_source_rgb(0, 0.8, 0)
-		content.rectangle(0, 0, width, height)
-		content.fill()
+	def expose(self, widget, event):
+		width, height = widget.window.get_size()
+		self.cr = cr = widget.window.cairo_create()
 		
-		content.set_source_rgb(1, 0, 1)
-		for x in range(int(ceil(net.x[0].rangeVal()))):
-			content.move_to(x * width / net.x[0].rangeVal(), 0)
-			content.line_to(x * width / net.x[0].rangeVal(), height)
-		content.stroke()
+		cr.set_source_rgb(1, 1, 1)
+		cr.rectangle(0, 0, width, height)
+		cr.fill()
 		
-		content.set_source_rgb(0.2, 4, 0.6)
-		for y in range(int(ceil(net.x[1].rangeVal()))):
-			content.move_to(0, y * height / net.x[1].rangeVal())
-			content.line_to(width, y * height / net.x[1].rangeVal())
-		content.stroke()
+		self.width = width - 45
+		self.height = height - 25
+		
+		self.drawGrid()
+		for i in range(self.app.maxLearn):
+			self.drawPoint(self.app.net.learning[i])
+	
+	def drawGrid(self):
+		cr = self.cr; width = self.width; height = self.height
+		net = self.app.net
+	
+		cr.set_source_rgb(0, 0, 0)
+		cr.select_font_face("Sans", cairo.FONT_SLANT_NORMAL,
+			cairo.FONT_WEIGHT_NORMAL)
+		cr.set_font_size(15)
+		
+		for x in range(11):
+			cr.move_to(x * width / 10, 0)
+			cr.rel_line_to(0, height)
+			cr.rel_move_to(0, 20)
+			cr.show_text(str(x / 10.0 * net.x[0].rangeVal()))
+		
+		for y in range(11):
+			cr.move_to(0, y * height / 10)
+			cr.rel_line_to(width, 0)
+			cr.rel_move_to(5, 0)
+			cr.show_text(str(y / 10.0 * net.x[1].rangeVal()))
+
+		cr.stroke()
+	
+	def drawPoint(self, point):
+		cr = self.cr; width = self.width; height = self.height
+		net = self.app.net
+		
+		x = (point[0] - net.x[0].minVal) / net.x[0].rangeVal() * width
+		y = (point[1] - net.x[1].minVal) / net.x[1].rangeVal() * height
+		radius = min(width, height) * 0.03
+		color = [0, 1, 0] if point[2] == 1 else [1, 0, 0]
+		
+		cr.set_source_rgb(*color)
+		cr.arc(x, y, radius, 0, 2 * pi)
+		cr.fill()
 
 
-try:
-	inputFilename = sys.argv[1]
-except:
-	print "použití: " + sys.argv[0] + " INPUT_FILE [OUTPUT_FILE]"
-	sys.exit(1)
+class App(gtk.Window):
+	
+	maxLearn = 0
+	maxTest = 0	
+	
+	def __init__(self, net):
+		super(App, self).__init__()
+		
+		self.net = net
+		
+		self.set_title("Simulace perceptronu neuronové sítě")
+		self.connect("delete-event", gtk.main_quit)
+		self.set_border_width(5)
+		
+		vbox = gtk.VBox(False, 5)
+		self.add(vbox)
+		
+		self.graph = graph = Graph()
+		graph.app = self
+		graph.connect("expose-event", graph.expose)
+		graph.set_size_request(400, 400)
+		vbox.pack_start(graph, True)
+		
+		hbox = gtk.HBox(False, 5)
+		hbox.set_size_request(0, 30)
+		vbox.pack_start(hbox, False)
+		
+		self.nextLearningButton = gtk.Button("Pokračovat v učení")
+		self.nextLearningButton.connect("clicked", self.nextLearning)
+		hbox.add(self.nextLearningButton)
+		
+		self.nextTestingButton = gtk.Button("Další testovací")
+		self.nextTestingButton.connect("clicked", self.nextTesting)
+		hbox.add(self.nextTestingButton)
+		
+		restartButton = gtk.Button("Začít znovu")
+		restartButton.connect("clicked", self.restart)
+		hbox.add(restartButton)
+		
+		self.show_all()
+	
+	def nextLearning(self, widget):
+		if self.maxLearn < self.net.learningLen:
+			self.maxLearn += 1
+		if self.maxLearn >= self.net.learningLen:
+			self.nextLearningButton.set_sensitive(False)
+		self.queue_draw()
+	
+	def nextTesting(self, widget):
+		if self.maxTest < self.net.testingLen:
+			self.maxTest += 1
+		if self.maxTest >= self.net.testingLen:
+			self.nextTestingButton.set_sensitive(False)
+		self.queue_draw()
+	
+	def restart(self, widget):
+		self.maxLearn = self.maxTest = 0
+		self.nextLearningButton.set_sensitive(True)
+		self.nextTestingButton.set_sensitive(True)
+		self.queue_draw()
 
-net = Net()
-net.readInput(inputFilename)
 
-window = gtk.Window()
-window.set_title("Neuron network perceptron simulation")
-window.connect("delete-event", gtk.main_quit)
+if __name__ == "__main__":
+	try:
+		inputFilename = sys.argv[1]
+	except:
+		print "použití: " + sys.argv[0] + " INPUT_FILE [OUTPUT_FILE]"
+		sys.exit(1)
 
-area = Graph()
-window.add(area)
+	inputNet = Net()
+	inputNet.readInput(inputFilename)
 
-window.show_all()
-gtk.main()
+	App(inputNet)
+	gtk.main()
 
